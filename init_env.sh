47778ee71eefe1d2d0db5e41f28f238ba082ef8b
shenben <1728816406@qq.com>
2024-09-27 07:36:52 +0000

    initiate the basic OS package

---
+++ init_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt update && apt install sudo
+
+sudo apt install -y build-essential autoconf software-properties-common apt-transport-https wget  cmake libboost-all-dev vim git curl 
+
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt update
+sudo apt install libgl1-mesa-glx pkg-config intel-oneapi-vtune
+
+# download_OpenVINO_2024 
+curl -L https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.3/linux/l_openvino_toolkit_ubuntu22_2024.3.0.16041.1e3b88e4e3f_x86_64.tgz --output openvino_2024.3.0.tgz
+tar -xf openvino_2024.3.0.tgz
+sudo mv l_openvino_toolkit_ubuntu22_2024.3.0.16041.1e3b88e4e3f_x86_64 /opt/intel/openvino_2024.3.0
+cd /opt/intel/openvino_2024.3.0
+sudo -E ./install_dependencies/install_openvino_dependencies.sh
+source /opt/intel/openvino_2024.3.0/setupvars.sh
---
+++ update.sh
@@ -0,0 +1,5 @@
+# git reset --hard 1b96a14908e28ab65369079eeb25220f1f8006d3
+# git push origin HEAD --force
+git add .
+git commit -m "initiate the basic OS package" 
+git push
