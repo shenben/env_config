8c8e2fefd49c41f6f33c586d2b724900e346db89
DISCOPASS <mackunnk@gmail.com>
2024-09-30 22:09:44 +0800

    plot the data

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/intel-iot-devkit/sample-videos.git
+# git clone https://github.com/intel-iot-devkit/sample-videos.git
 
 # /usr/bin/time -v /home/pxg/openvino_cpp_samples_build/intel64/Release/classification_sample_async  \
 #  -i /home/pxg/video_analytics/vet/SmolCat.mp4/bmp_images \
@@ -9,5 +9,8 @@ git clone https://github.com/intel-iot-devkit/sample-videos.git
 
 /usr/bin/time -v /root/openvino_cpp_samples_build/intel64/Release/classification_sample_async   \
  -i /tmp/traffic.mp4   \
- -m /root/env_config/googlenet-v1/FP16/googlenet-v1.xml \
+ -m /root/env_config/googlenet-v1/FP32/googlenet-v1.xml \
  -d CPU
+
+# vtune -collect uarch-exploration -result-dir vtune_results_FP16 -- /root/openvino_cpp_samples_build/intel64/Release/classification_sample_async -i /tmp/traffic.mp4 -m /root/env_config/googlenet-v1/FP16/googlenet-v1.xml -d CPU
+# vtune -report summary -r vtune_results_FP16 -format=csv -report-output=summary_FP16.csv 
