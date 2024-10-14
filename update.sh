a1fa31d0b3eb627c9d056bd195423186589f6269
shenben <1728816406@qq.com>
2024-10-14 07:34:07 +0000

    Set the font to be embedded

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # git reset --hard 1b96a14908e28ab65369079eeb25220f1f8006d3
 # git push origin HEAD --force
 git add .
-git commit -m "DNN ipynb sample" 
+git commit -m "Set the font to be embedded" 
 git push
