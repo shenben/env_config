304bd641bbea2b100be8c46b91b87d4cdc1e5249
shenben <yc27978@um.edu.mo>
2024-10-20 20:40:34 +0800

    intel-oneapi-vtune offline package

--- init_env.sh
+++ init_env.sh
@@ -4,11 +4,11 @@ apt update && apt install sudo
 
 sudo apt install -y build-essential autoconf software-properties-common apt-transport-https wget  cmake libboost-all-dev vim git curl 
 
-wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-sudo apt update
-sudo apt install libgl1-mesa-glx pkg-config intel-oneapi-vtune
+# wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+# sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+# echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+# sudo apt update
+# sudo apt install libgl1-mesa-glx pkg-config intel-oneapi-vtune
 
 # download_OpenVINO_2024 
 curl -L https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.3/linux/l_openvino_toolkit_ubuntu22_2024.3.0.16041.1e3b88e4e3f_x86_64.tgz --output openvino_2024.3.0.tgz
@@ -24,5 +24,7 @@ source /opt/intel/openvino_2024.3.0/setupvars.sh
 # bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 
 git clone https://github.com/oneapi-src/oneAPI-samples.git
-pip install matplotlib py-cpuinfo intel_extension_for_pytorch==1.13.100 torch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 t
-ransformers transformers sentencepiece sacremoses
+pip install matplotlib py-cpuinfo intel_extension_for_pytorch==1.13.100 torch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 transformers transformers sentencepiece sacremoses
+
+wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/d7e1fdb1-cfc7-40fb-bf46-3719e9372d67/l_oneapi_vtune_p_2024.3.0.31_offline.sh
+sudo sh ./l_oneapi_vtune_p_2024.3.0.31_offline.sh
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # git reset --hard 1b96a14908e28ab65369079eeb25220f1f8006d3
 # git push origin HEAD --force
 git add .
-git commit -m "update 1" 
+git commit -m "intel-oneapi-vtune offline package" 
 git push
